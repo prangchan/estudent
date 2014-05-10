2dc2e031e97a7437075fbe5ff76807555d3c0b0d
TOSHIBA <TOSHIBA@TOSHIBA-PC>
2014-05-10 16:18:50 +0700

    ปรับขนาดหน้าจอ

---
+++ public/login.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>
+loging
+</title>
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+</head>
+<body>
+<div class="container">
+<div class="col-sm-2 col-md-3">  </div>
+<div class="col-sm-7 col-md-5"> 
+<div class="panel panel-default">
+ <div class="panel-heading">Panel heading without title</div>
+  <div class="panel-body"> Panel content</div>    
+  </div>
+  </div>
+  <div class="col-sm-3 col-md-4">  </div>
+  </div> 
+</body>
+</html>
+
